3b82fef1b77f46c15d75600cd572a0c90cda9295
WRATHeXe <FardinRuhian001@gmail.com>
2025-03-16 00:14:09 +0600

    added navbar, hero and footer section with backend of login

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+function login() {
+    const username = document.getElementById('user').value;
+    const password = document.getElementById('password').value;
+    console.log(password);
+    
+    if (password === "123456") { 
+        alert(`Welcome, ${username}!`);
+        document.getElementById('hero').style.display = 'none';
+    } else {
+        alert('Wrong password! Please try again.');
+    }
+}
